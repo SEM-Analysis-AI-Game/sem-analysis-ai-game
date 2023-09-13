0b7578add7f4ae73c4a1cd8aeab4ac2656770f5b
John Siegel <jpsiegel8@gmail.com>
2023-09-13 12:58:46 -0400

    Updated eslint config

--- apps/game/vite.config.ts
+++ apps/game/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import eslint from 'vite-plugin-eslint';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), eslint()],
