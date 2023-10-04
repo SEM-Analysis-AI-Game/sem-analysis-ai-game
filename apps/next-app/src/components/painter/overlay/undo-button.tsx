f18ebc58d0b124934847198e9836e3512a8a2343
Anthony Nguyen <anthonyn33@vt.edu>
2023-10-04 17:09:09 -0400

    Added rest of files

---
+++ apps/next-app/src/components/painter/overlay/undo-button.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useActionHistory } from "../action-history";
+
+export function UndoButton(): JSX.Element {
+    const history = useActionHistory();
+
+    return (
+        <button
+            className="text-[#333] bg-slate-100 rounded pl-2 pr-2 mt-1 mb-1 border-black border-2"
+            key="undo-button"  
+            onClick={() => history.undo()}  
+        >
+            Undo
+        </button>
+    );
+}
