86da8e59732d24c0b58d81f12bf10a167bc0d97c
John Siegel <jpsiegel8@gmail.com>
2023-10-03 20:31:22 -0400

    Refactored and documented shaders

--- apps/next-app/src/components/painter/shaders.ts
+++ apps/next-app/src/components/painter/shaders.ts
@@ -1,5 +1,8 @@
 import { kPanMultiplier } from "./tools";
 
+/**
+ * This shader does nothing but pass the texture through.
+ */
 export const vertexShader = `
 varying vec3 vUv;
 
@@ -9,37 +12,74 @@ void main() {
     gl_Position = projectionMatrix * modelViewPosition; 
 }`;
 
-export const backgroundFragmentShader = `
-uniform sampler2D background;
-uniform float zoom;
+/**
+ * Converts a number to GLSL float type.
+ */
+function toGLSLFloat(n: number) {
+  if (Number.isInteger(n)) {
+    return n.toFixed(1);
+  } else {
+    return n;
+  }
+}
+
+/**
+ * This shader function applies pan and zoom to screen coordinates, and then
+ * converts them to texture coordinates.
+ */
+const kApplyPanAndZoom = `
 uniform vec2 pan;
+uniform float zoom;
+void applyPanAndZoom(in vec2 uv, out vec2 transformedCoords) {
+  transformedCoords = ((uv / sqrt(zoom)) + pan * ${toGLSLFloat(
+    kPanMultiplier
+  )}) * 0.5 + 0.5;
+}
+`;
+
+/**
+ * This shader applies pan and zoom to the background texture.
+ */
+export const backgroundFragmentShader = `
 varying vec3 vUv;
 
+uniform sampler2D background;
+
+${kApplyPanAndZoom}
+
 void main() {
-    vec2 transformedCoords = ((vUv.xy / sqrt(zoom)) + pan * ${kPanMultiplier.toFixed(
-      1
-    )}) * 0.5 + 0.5;
+    vec2 transformedCoords = vec2(0.0);
+    applyPanAndZoom(vUv.xy, transformedCoords);
     gl_FragColor = texture2D(background, transformedCoords);
 }`;
 
+/**
+ * Builds a fragment shader for rendering a section of the drawing layer.
+ *
+ * Pan and zoom are applied.
+ *
+ * @param size The size of the section to render.
+ * @param position The position of the section to render.
+ */
 export function buildFragmentShader(
   size: THREE.Vector2,
   position: THREE.Vector2
 ) {
   return `
-uniform sampler2D inputDiffuse;
-uniform float zoom;
-uniform vec2 pan;
 varying vec3 vUv;
+
+uniform sampler2D inputDiffuse;
+
+${kApplyPanAndZoom}
 
 void main() {
     gl_FragColor = vec4(0.0);
 
-    vec2 transformedCoords = ((vUv.xy / sqrt(zoom)) + pan * ${kPanMultiplier.toFixed(
-      1
-    )}) * 0.5 + 0.5;
-    vec2 min = vec2(float(${position.x}), float(${position.y}));
-    vec2 max = min + vec2(float(${size.x}), float(${size.y}));
+    vec2 transformedCoords = vec2(0.0);
+    applyPanAndZoom(vUv.xy, transformedCoords);
+
+    vec2 min = vec2(${toGLSLFloat(position.x)}, ${toGLSLFloat(position.y)});
+    vec2 max = min + vec2(${toGLSLFloat(size.x)}, ${toGLSLFloat(size.y)});
 
     if (transformedCoords.x > min.x && transformedCoords.x < max.x && transformedCoords.y > min.y && transformedCoords.y < max.y) {
         transformedCoords = (transformedCoords - min) / (max - min);
