b3ef936dca7299507aa69c26332329bd3033b503
Alex Lin <38541529+alxn3@users.noreply.github.com>
2023-10-17 14:18:06 -0400

    Backend stuff

--- apps/sam-microservice/server.py
+++ apps/sam-microservice/server.py
@@ -1,14 +1,26 @@
 import atexit
-from flask import Flask, send_from_directory
+from flask import Flask, request, app
 from ultralytics import SAM
 
 # Load the model
-model = SAM('mobile_sam.pt')
+model = SAM("mobile_sam.pt")
 
 # Predict a segment based on a point prompt
-result = model.predict('sem.png', points=[900, 370], labels=[1])
+# result = model.predict("sem.png", save=True, save_crop=True)
 
-print(result)
+# print(result)
+
+
+@app.route("/", methods=["POST"])
+def segment():
+    # for now mock out result and return JSON something blah blah blah
+    # get image from request
+    # run image through predictor
+    # return result
+    image = request.files["image"]
+    result = model(image)
+    return result.Masks
+
 
 # app = Flask(__name__)
 
