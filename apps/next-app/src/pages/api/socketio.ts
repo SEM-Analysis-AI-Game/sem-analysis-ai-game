1c74dcbfe08ccbc81bccd66b5784394d34e7321a
John Siegel <jpsiegel8@gmail.com>
2023-10-21 18:03:20 -0400

    Properly joining rooms

--- apps/next-app/src/pages/api/socketio.ts
+++ apps/next-app/src/pages/api/socketio.ts
@@ -4,7 +4,7 @@ import { Server as HttpServer } from "http";
 import { Server as NetServer, Socket } from "net";
 import { NextApiResponse } from "next";
 import { Server as SocketIOServer } from "socket.io";
-import { DrawEvent } from "@/util";
+import { DrawEvent, kImages } from "@/util";
 
 export const config = {
   api: {
@@ -30,19 +30,37 @@ export default async function socket(
     });
 
     io.on("connection", (connection) => {
+      const url = connection.request.headers.referer;
+      const imageIndex = url?.slice(url.lastIndexOf("/") + 1);
+      if (imageIndex && imageIndex !== "") {
+        connection.join(imageIndex);
+      }
       connection.on("draw", async (data: DrawEvent) => {
-        connection.broadcast.emit("draw", data);
-        // TODO: update the imageIndex param so we use the correct room/image for the user.
-        await fetch(
-          `http://localhost${
-            process.env.PORT ? `:${process.env.PORT}` : ""
-          }/api/state?imageIndex=${0}`,
-          {
-            method: "POST",
-            body: JSON.stringify(data),
-            cache: "no-cache",
-          }
-        );
+        let room = undefined;
+        for (const connectedRoom of connection.rooms) {
+          try {
+            const roomInt = parseInt(connectedRoom);
+            if (roomInt >= 0 && roomInt < kImages.length) {
+              room = connectedRoom;
+              break;
+            }
+          } catch (_) {}
+        }
+        if (room) {
+          connection.broadcast.to(room).emit("draw", data);
+          await fetch(
+            `http://localhost${
+              process.env.PORT ? `:${process.env.PORT}` : ""
+            }/api/state?imageIndex=${imageIndex}`,
+            {
+              method: "POST",
+              body: JSON.stringify(data),
+              cache: "no-cache",
+            }
+          );
+        } else {
+          throw new Error("Draw event from user not in a room");
+        }
       });
     });
 
