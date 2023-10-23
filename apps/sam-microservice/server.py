c32c7ab77e5ce8d31df618e7932eb59ff4c29d02
lukedigiovanna <lukedigiovanna@gmail.com>
2023-10-22 22:35:57 -0400

    SAM stuff

--- apps/sam-microservice/server.py
+++ apps/sam-microservice/server.py
@@ -1,41 +1,56 @@
 import atexit
-from flask import Flask, request, app
+from flask import Flask, request
 from ultralytics import SAM
+
+import requests
+from PIL import Image
+from io import BytesIO
+
+# Step 1: Download the image from the URL
+def download_image(url):
+    response = requests.get(url)
+    image = Image.open(BytesIO(response.content))
+    return image
 
 # Load the model
 model = SAM("mobile_sam.pt")
 
-# Predict a segment based on a point prompt
-# result = model.predict("sem.png", save=True, save_crop=True)
-
-# print(result)
-
+app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def segment():
-    # for now mock out result and return JSON something blah blah blah
     # get image from request
     # run image through predictor
     # return result
-    image = request.files["image"]
+    # image = request.files["image"]
+    body = request.json
+
+    n_fields = ['points', 'imageURL']
+
+    if len(list(filter(lambda f: f not in body, n_fields))) > 0:
+        return 'Missing a required field', 400
+
+    points, url = body['points'], body['imageURL']
+
+    image = download_image(url)
+
+    # download the image from the url
+
     result = model(image)
-    return result.Masks
 
+    print(result)
 
-# app = Flask(__name__)
+    print(url)
+    print(points)
 
-# @app.route('/')
-# def segment():
+    # print(image)
+    # result = model(image)
+    # return result.Masks
+    return {"mask": []}
 
+def shutdown():
+    print("Flask is shutting down...")
 
-#     # for now mock out result and return JSON something blah blah blah
-#     return {
-#         "result": "Hello"
-#     }
+atexit.register(shutdown)
 
-# def shutdown():
-#     print("Flask is shutting down...")
-
-# atexit.register(shutdown)
-
-# app.run()
+app.run()
