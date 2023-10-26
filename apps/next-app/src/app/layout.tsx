968d26f2d6d09e8821d7ee4274468cf7469c5080
Alex Lin <38541529+alxn3@users.noreply.github.com>
2023-10-25 21:29:14 -0400

    Style

--- apps/next-app/src/app/layout.tsx
+++ apps/next-app/src/app/layout.tsx
@@ -3,6 +3,12 @@ import type { Metadata } from "next";
 import { SocketConnectionProvider } from "@/components";
 import { PropsWithChildren } from "react";
 
+import { Montserrat } from "next/font/google";
+
+const font = Montserrat({
+  subsets: ["latin"],
+});
+
 export const dynamicParams = false;
 
 export const metadata: Metadata = {
@@ -12,7 +18,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout(props: PropsWithChildren) {
   return (
-    <html lang="en">
+    <html lang="en" className={font.className}>
       <body className="bg-slate-50">
         <SocketConnectionProvider>{props.children}</SocketConnectionProvider>
       </body>
--- apps/next-app/src/app/page.tsx
+++ apps/next-app/src/app/page.tsx
@@ -1,37 +1,54 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import { kImages, SEMImageData } from "@/util";
+import { Gradient } from "@/components/Gradient";
+import { useEffect } from "react";
 
 export default async function Home(): Promise<JSX.Element> {
+  useEffect(() => {
+    const gradient = new Gradient();
+    gradient.initGradient("#gradient-canvas");
+  }, []);
   return (
     <>
-      <div className="background" />
-      <h1 className="text-center font-bold text-2xl mt-10">
-        Behind Density Lines
-      </h1>
-      <div className="max-w-xl block mx-auto mt-5 overflow-y-scroll">
-        <p>
+      <div>
+        <canvas
+          id="gradient-canvas"
+          className="w-full h-full top-0 left-0 opacity-30 -z-10 fixed"
+          data-transition-in
+        />
+      </div>
+      <div className="py-10 w-[min(120rem,90%)] mx-auto text-center space-y-10 ">
+        <h1 className="text-center font-light text-[min(8rem,6vw)] mt-10 opacity-80">
+          Behind Density Lines
+        </h1>
+        <p className="font-medium text-2xl">
           Choose one of the images below to start collaboratively segmenting:
         </p>
-        {kImages.map((data: SEMImageData, index: number) => (
-          <Link href={`/${index}`} key={index} className="border rounded p-4 flex transition bg-[rgba(240,240,240,0.9)] hover:opacity-80 my-2 active:bg-blue">
-            <Image 
-              width={100}
-              height={100}
-              src={data.image.src}
-              alt=""
-              className="rounded"
-            />
-            <div className="ml-4">
-              <h1 className="font-bold">
-                Room: {index + 1}
-              </h1>
-              <p>
-                { data.description }
-              </p>
-            </div>
-          </Link>
-        ))}
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] gap-4 bg-slate-50 p-4 rounded-md">
+          {kImages.map((data: SEMImageData, index: number) => (
+            <Link
+              href={`/${index}`}
+              key={index}
+              className="border rounded p-4 flex flex-col gap-2 transition bg-[rgba(240,240,240,0.9)] hover:opacity-80 active:bg-blue"
+            >
+              <div className="w-full relative pt-[100%]">
+                <Image
+                  fill
+                  src={data.image.src}
+                  alt=""
+                  className="rounded w-full h-full object-cover top-0 left-0"
+                />
+              </div>
+              <div className="ml-4">
+                <h1 className="font-bold">Room: {index + 1}</h1>
+                <p>{data.description}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </>
   );
