9822484702a823492074af5ab6dcdf2098952d39
lukedigiovanna <lukedigiovanna@gmail.com>
2023-11-07 14:33:24 -0500

    score function

---
+++ apps/next-app/src/server/scoring.ts
@@ -0,0 +1,10 @@
+/**
+ * @param correct The 
+ */
+export const scoringFunction = (resolution: [number, number], correct: number[], given: number[]) => {
+    const [width, height] = resolution; 
+
+    // compare correct vs given and return a number from 0 to 1
+
+    return 0;
+}
