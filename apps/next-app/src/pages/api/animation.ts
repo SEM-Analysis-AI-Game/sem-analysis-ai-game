5d44bae12b471b912474711fffd967d026530ac3
John Siegel <jpsiegel8@gmail.com>
2023-11-08 10:02:59 -0500

    Removed unneeded clone

--- apps/next-app/src/pages/api/animation.ts
+++ apps/next-app/src/pages/api/animation.ts
@@ -1,17 +1,12 @@
 import { serverState } from "@/server";
 import { NextApiRequest, NextApiResponse } from "next";
-// @ts-ignore
-import GIFEncoder from "gif-encoder-2";
 
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
   const state = serverState[parseInt(request.query.imageIndex as string)];
-  const copiedEncoder = {
-    ...state.gifEncoder,
-  };
-  const data = new Uint8Array(copiedEncoder.out.getData());
+  const data = new Uint8Array(state.gifEncoder.out.getData());
   return response
     .status(200)
     .setHeader("Content-Type", "image/gif")
