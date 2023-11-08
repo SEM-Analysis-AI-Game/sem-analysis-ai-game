09d393c907262d0b29754ef9931864f107687408
John Siegel <jpsiegel8@gmail.com>
2023-11-08 18:30:43 -0500

    Pruned old dirs

--- tif2png.py
+++
@@ -1,7 +0,0 @@
-import os, cv2
-
-# convert tif to png
-for file in os.listdir("sem-images"):
-    filename = "sem-images/" + os.fsdecode(file)
-    image = cv2.imread(filename)
-    cv2.imwrite(filename[:-4] + ".png", image)
