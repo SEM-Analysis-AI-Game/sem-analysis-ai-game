72a0f6ed09e343cfb99bbc18b90871cca71d9ef5
lukedigiovanna <lukedigiovanna@gmail.com>
2023-11-09 00:33:03 -0500

    latest scoring changes

--- apps/next-app/src/pages/api/score.ts
+++ apps/next-app/src/pages/api/score.ts
@@ -4,9 +4,25 @@ import { NextApiRequest, NextApiResponse } from "next";
 // @ts-ignore
 import GIFEncoder from "gif-encoder-2";
 
-const references: {[key: number]: number[]} = {
-    1: []
+type EncodingSegment = readonly [null | number, number];
+
+// Store run length encoded version to preserve space
+const referencesRLE: {[key: number]: EncodingSegment[]} = {
+    1: [[null,262216],[0,5],[null,726],[0,7],[null,724],[0,9],[null,723],[0,9],[null,723],[0,9],[null,3],[0,11],[null,709],[0,9],[null,1],[0,17],[null,705],[0,28],[null,704],[0,29],[null,703],[0,30],[null,702],[0,32],[null,700],[0,41],[null,691],[0,42],[null,689],[0,44],[null,688],[0,44],[null,688],[0,44],[null,688],[0,44],[null,688],[0,44],[null,688],[0,43],[null,689],[0,42],[null,690],[0,42],[null,690],[0,41],[null,690],[0,42],[null,690],[0,41],[null,691],[0,41],[null,691],[0,41],[null,691],[0,41],[null,691],[0,41],[null,691],[0,40],[null,692],[0,40],[null,692],[0,40],[null,692],[0,39],[null,693],[0,39],[null,693],[0,39],[null,693],[0,38],[null,694],[0,38],[null,695],[0,36],[null,697],[0,34],[null,698],[0,34],[null,699],[0,32],[null,701],[0,30],[null,704],[0,27],[null,707],[0,24],[null,710],[0,21],[null,713],[0,18],[null,717],[0,13],[null,241376]]
+
+
+};
+
+const references: {[key: number]: (number | null)[]} = {};
+for (const key in referencesRLE) {
+    references[key] = [];
+    for (const [value, count] of referencesRLE[key]) {
+        for (let i = 0; i < count; i++) {
+            references[key].push(value);
+        }
+    }
 }
+// console.log(references)
 
 export default async function handler(
   request: NextApiRequest,
@@ -16,27 +32,12 @@ export default async function handler(
     const state = serverState[imageIndex];
     const segments = state.canvas.map(value => value.segment);
     const dimension = state.resolution;
-    const score = scoringFunction([5, 5], [
-        0, 0, 1, 1, 0,
-        0, 1, 1, 1, 1,
-        0, 0, 1, 0, 0,
-        0, 2, 0, 0, 0, 
-        0, 2, 0, 0, 0
-    ], [
-        0, 0, 2, 2, 0,
-        0, 0, 0, 0, 0,
-        0, 1, 0, 0, 0,
-        0, 1, 0, 0, 0,
-        0, 1, 0, 0, 0,
-    ]);
 
     if (!Object.hasOwn(references, imageIndex)) {
         throw Error(`There is no reference segmentation for image index ${imageIndex}`);
     }
 
-    // const score = scoringFunction(dimension, references[imageIndex], segments);
-
-    // const score = 0.58342;
+    const score = scoringFunction(dimension, references[imageIndex], segments);
 
     return response
         .status(200)
--- apps/next-app/src/server/scoring.ts
+++ apps/next-app/src/server/scoring.ts
@@ -3,7 +3,7 @@
  * @param reference Segment index array of correct/reference image
  * @param input Segement index array of a given image to assign a score to
  */
-export const scoringFunction = (resolution: readonly [number, number], reference: readonly number[], input: readonly number[]) => {
+export const scoringFunction = (resolution: readonly [number, number], reference: readonly (number | null)[], input: readonly (number | null)[]) => {
     /*
     Approach:
         Perform an optimal mapping of correct segments to given segments. This mapping should be injective
@@ -22,33 +22,46 @@ export const scoringFunction = (resolution: readonly [number, number], reference
 
     // input segment index to a map that associates a reference segment index to the count
     const counts = new Map<number, Map<number, number>>();
-    const found = new Array<boolean>(size);
+    const found = new Array<boolean>(size).fill(false);
     let numReferencePoints = 0;
+    let refseg = 0;
+    let wrongfill = 0;
+    let vals = [];
     for (let i = 0; i < size; i++) {
-        if (reference[i] && reference[i] !== 0) {
+        if (reference[i] !== null) {
             numReferencePoints++;
+            refseg++;
         }
-        else if (!input[i] || input[i] === 0) {
-            numReferencePoints++; // penalty for filling in when they shouldn't have
+        else if (input[i] !== undefined) { // i.e. reference is null and the user segmented something there
+            vals.push(input[i])
+            numReferencePoints++;
+            wrongfill++;
+        }
+        
+        if (input[i] === undefined) {
             continue;
         }
         
-        if (found[i]) continue;
+        if (found[i]) {
+            continue;
+        }
 
         let [x, y] = [i % width, Math.floor(i / width)];
         // perform DFS
         const stack: number[] = [i];
+        console.log('starting dfs on', i)
         found[i] = true;
         while (stack.length > 0) {
             const n = stack.pop() as number;
             const segmentIndex = input[n];
             const referenceIndex = reference[n];
-            if (referenceIndex) {
-                if (!counts.has(segmentIndex)) {
-                    counts.set(segmentIndex, new Map<number, number>());
+            console.log(segmentIndex, referenceIndex);
+            if (referenceIndex !== null) {
+                if (!counts.has(segmentIndex as number)) {
+                    counts.set(segmentIndex as number, new Map<number, number>());
                 }
 
-                const countMap = counts.get(segmentIndex);
+                const countMap = counts.get(segmentIndex as number);
                 if (countMap?.has(referenceIndex)) {
                     countMap.set(referenceIndex, countMap.get(referenceIndex) as number + 1);
                 }
@@ -61,14 +74,25 @@ export const scoringFunction = (resolution: readonly [number, number], reference
                 const [nx, ny] = [x + dir[0], y + dir[1]];
                 if (nx >= 0 && nx < width && ny >= 0 && ny < height) {
                     const index = nx + ny * width;
+                    console.log('exploring',index,input[index],found[index],segmentIndex);
                     if (!found[index] && input[index] === segmentIndex) {
                         stack.push(index);
+                        console.log(stack);
                         found[index] = true;
                     }
                 }
             }
         }
     }
+
+    console.log('refseg', refseg);
+    console.log('wrongfill', wrongfill);
+    console.log(vals);
+
+    console.log("Counts");
+    counts.forEach((value => {
+        console.log(Object.fromEntries(value))
+    }))
 
     // FOR OPTIMAL SOLUTION:
     // To perfrom an optimal matching that associates reference segment indexes to input
@@ -94,16 +118,21 @@ export const scoringFunction = (resolution: readonly [number, number], reference
         }
     });
 
+    console.log("Reference map")
+    console.log(Object.fromEntries(inputToReference));
+
     let correctCount = 0;
 
     counts.forEach((value: Map<number ,number>, inputIndex: number) => {
         const referenceIndex = inputToReference.get(inputIndex);
-        if (referenceIndex) {
+        if (referenceIndex !== null && referenceIndex !== undefined) {
             correctCount += value.get(referenceIndex) as number;
         }
     });
 
     const totalCorrect = correctCount;
+
+    console.log(totalCorrect, '/', numReferencePoints);
 
     const score = totalCorrect / numReferencePoints;
 
