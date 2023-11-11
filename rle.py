86182077561ef11e56af87f73fcae864c6a03aa3
lukedigiovanna <lukedigiovanna@gmail.com>
2023-11-10 22:56:56 -0500

    Update rle.py to match json format

--- rle.py
+++ rle.py
@@ -1,9 +1,19 @@
+import sys
+import json
+
+if len(sys.argv) != 2:
+    print("Incorrect usage - input the file name to run-length encode")
+    exit()
+
+file = sys.argv[1]
+
 print('[', end='')
-with open("1.txt", "r") as f:
-    lines = map(lambda x: x.strip()[:-1], f.readlines())
+with open(file, "r") as f:
+    # lines = map(lambda x: x.strip()[:-1], f.readlines())
+    js = json.loads(f.read())
     last = None
     length = 0
-    for l in lines:
+    for l in js["segments"]:
         if l != last:
             if last is not None:
                 print(f'[{last},{length}]',end=',')
